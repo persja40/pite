d15a78a4e5114afa0408da11a9cf71e7061bc8e3
persja40 <persja40@gmail.com>
2018-04-13 08:24:58 +0200

    simple login

--- blabla/blabla/settings.py
+++ blabla/blabla/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'blabla.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'), ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,3 +122,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = 'home'
